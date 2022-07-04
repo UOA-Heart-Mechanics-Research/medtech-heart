ba8ad3d307e4f66b8a1aa80c1e09ab7a92443dd2
LinkunGao <gaolinkun123@gmail.com>
2022-07-05 09:53:54 +1200

    build project

--- latest/_nuxt/static/1656397842/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmedtech-heart\u002Flatest\u002F_nuxt\u002Fstatic\u002F1656397842",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002Fmedtech-heart\u002Flatest\u002F",assetsPath:"\u002Fmedtech-heart\u002Flatest\u002F_nuxt\u002F",cdnURL:a}}}}(null));
