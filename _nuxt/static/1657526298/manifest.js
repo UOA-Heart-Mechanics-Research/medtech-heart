01b3ef32be7de8cde7156d7bd58a0325d91d9026
LinkunGao <gaolinkun123@gmail.com>
2022-07-12 11:39:54 +1200

    add google analytics

--- _nuxt/static/1657526298/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fattack-healthy","\u002Fmodel-heart","\u002Fattack-minor","\u002Ffailure-compensated","\u002Felectricity-healthy","\u002Felectricity-fibrillation","\u002Fattack-severe","\u002Fmodel-heart#video-div","\u002Ffailure-healthy","\u002Ffailure-decompensated","\u002Fvideo","\u002Fabout"]})
