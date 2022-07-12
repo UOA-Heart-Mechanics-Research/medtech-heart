001cff9d421b0672448764d1312353720b433384
LinkunGao <gaolinkun123@gmail.com>
2022-07-12 16:14:59 +1200

    add ga4

--- _nuxt/static/1657582552/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fmodel-heart#video-div","\u002Fattack-severe","\u002Fattack-healthy","\u002Felectricity-healthy","\u002Ffailure-healthy","\u002Fmodel-heart","\u002Fattack-minor","\u002Ffailure-decompensated","\u002Ffailure-compensated","\u002Fabout","\u002Fvideo","\u002Felectricity-fibrillation"]})
