359c77f34434e229356f33885ae8357f3cbbfee6
LinkunGao <gaolinkun123@gmail.com>
2022-08-20 14:29:01 +1200

    add three fingers to pan model function on trouchscreen

---
+++ _nuxt/static/1660962383/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fmodel-heart","\u002Fmodel-heart#video-div","\u002Felectricity-fibrillation","\u002Ffailure-healthy","\u002Ffailure-decompensated","\u002Fattack-minor","\u002Fattack-healthy","\u002Fattack-severe","\u002Ffailure-compensated","\u002Felectricity-healthy","\u002Fvideo","\u002Fabout"]})
