0ec7d7df228de68e1b499eb82827641212c408c4
LinkunGao <gaolinkun123@gmail.com>
2024-03-13 21:13:03 +1300

    add .bat file to start app via docker or locally start

--- docker-start.bat
+++ docker-start.bat
@@ -1,7 +1,10 @@
 @echo off
 cls
 type logo.txt
-echo Starting Medtech Heart App...
+echo Starting Medtech Heart App via Docker...
 echo.
 
+start "" "chrome.exe" "--start-fullscreen" "http://localhost:5173/model-heart"
+
 docker-compose -f docker-compose.yml up
+
