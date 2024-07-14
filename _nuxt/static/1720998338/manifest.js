3b3215d0950638a15cc67a15a5c363d84ecdacbb
LinkunGao <gaolinkun123@gmail.com>
2024-07-14 23:05:57 +0000

    deploy: b52e35f676dfeaee16c50518aed4baa3f4bb4432

---
+++ _nuxt/static/1720998338/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fabout","\u002Fattack-severe","\u002Fmodel-heart","\u002Fattack-healthy","\u002Felectricity-fibrillation","\u002Fvideo","\u002Fmodel-heart#video-div","\u002Ffailure-decompensated","\u002Ffailure-compensated","\u002Ffailure-healthy","\u002Fattack-minor","\u002Felectricity-healthy"]})
